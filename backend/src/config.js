b02937a2586461f273fb35babcaf28ae17a985ac
klxcoder <klxcoder@gmail.com>
2025-03-04 13:15:26 +0700

    config.js

---
+++ backend/src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: 'mysql2',
+  connection: {
+    host: 'db',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+  },
+}
--- backend/src/database.js
+++ backend/src/database.js
@@ -1,11 +1,5 @@
-const database = require('knex')({
-  client: 'mysql2',
-  connection: {
-    host: 'db',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-  },
-});
+const config = require('./config');
 
-module.exports = database
+const database = require('knex')(config);
+
+module.exports = database;
