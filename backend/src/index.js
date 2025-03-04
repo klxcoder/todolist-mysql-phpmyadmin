7549c046c23802be6b45240aa34b4defd5a59c92
klxcoder <klxcoder@gmail.com>
2025-03-04 13:08:11 +0700

    knex list tables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: 'db',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    // database: 'myapp_test',
+  },
+});
+
+async function getTables() {
+  const tables = await knex('information_schema.tables')
+    .select('table_name')
+    .where('table_schema', 'your_database_name');
+  console.log(tables.map(t => t.table_name));
+}
+
+getTables();
+
+
 // Respond with "Hello" at the root path
 app.get('/', (req, res) => {
   res.send('Hello from non-root container');
